df6f7501bd2758426491c6d6644aff6f8747811a
Anandan <44726326+vijay043004@users.noreply.github.com>
2025-11-24 09:51:39 +0100

    homePage / navbar
    page d'accueil et navbar faite

--- Bateau_Thibault/src/app/app.component.ts
+++ Bateau_Thibault/src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButtons, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButtons, IonButton],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router) {}
+
+    onGoToHome()
+  { this.router.navigate(['/home']) }
+
+  onGoToBateaux()
+  { this.router.navigate(['/bateau']) }
+
+  onGoToProduits()
+  { this.router.navigate(['/produits']) }
+
+  onGoToRestaurants()
+  { this.router.navigate(['/restaurants']) }
+
+  onGoToRecettes()
+  { this.router.navigate(['recettes']) }
+
+  onGoToContact()
+  { this.router.navigate(['/contact']) }
 }
--- Bateau_Thibault/src/app/pages/home/home.page.ts
+++ Bateau_Thibault/src/app/pages/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonMenu, IonButtons, IonMenuButton, IonButton, IonNav } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonMenu, IonButtons, IonMenuButton, IonButton, IonNav],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {}
+
+
+  onGoToHome()
+  { this.router.navigate(['/home']) }
+
+  onGoToBateaux()
+  { this.router.navigate(['/bateau']) }
+
+  onGoToProduits()
+  { this.router.navigate(['/produits']) }
+
+  onGoToRestaurants()
+  { this.router.navigate(['/restaurants']) }
+
+  onGoToRecettes()
+  { this.router.navigate(['recettes']) }
+
+  onGoToContact()
+  { this.router.navigate(['/contact']) }
 }
